5c8c283607eac950928e127e3f7c56f3aa3872ca
NathanTaylor1220 <nj20tayl@siena.edu>
2026-09-14 14:45:42 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -1,13 +1,14 @@
-
+import java.util.Scanner;
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Nathan Taylor
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Hometown:Greenville, Favorite Restaurant:Chilis, Favorite Item:Steak");
     }
 }
